a50171754c09efb1c0f6402a2f11d2850e60ea39
Florian Mauffrey <32743829+Mauffrey@users.noreply.github.com>
2022-12-01 06:24:07 +0100

    UI improvement with CustomTkinter

--- gui_inpainting.py
+++ gui_inpainting.py
@@ -1,24 +1,23 @@
 import os.path
-from tkinter import *
-from tkinter.ttk import *
-from tkinter import filedialog
 import tkinter as tk
+from tkinter import filedialog
 from diffusers import OnnxStableDiffusionInpaintPipeline
 import threading
 from modes import inpainting
 from PIL import ImageGrab, Image
+import customtkinter
 
 
 # Function for opening the file explorer window
-def browseFiles():
+def browse_files():
     filename = filedialog.askopenfilename(initialdir="/", title="Select a File",
                                           filetypes=(("Image png", "*.png*"), ("all files", "*.*")))
 
     # Update text input file
-    input_im_text_r.config(state="normal")
-    input_im_text_r.delete(0, "end")
-    input_im_text_r.insert(0, filename)
-    input_im_text_r.config(state="disabled")
+    input_im_text.configure(state="normal")
+    input_im_text.delete(0, "end")
+    input_im_text.insert(0, filename)
+    input_im_text.configure(state="disabled")
 
     # Resize image
     img = Image.open(filename)
@@ -27,34 +26,47 @@ def browseFiles():
 
     # Load in Canvas
     image = tk.PhotoImage(file="images/inpaint_input.png")
-    canvas_background.create_image(0, 0, anchor=NW, image=image)
+    canvas_background.create_image(0, 0, anchor="nw", image=image)
     image_input.append(image)
 
 
 def paint(event):
     x1, y1 = (event.x - 1), (event.y - 1)
     x2, y2 = (event.x + 1), (event.y + 1)
-    canvas_background.create_oval(x1, y1, x2, y2, fill=color.get(), outline=color.get(), width=size_value.get())
+    canvas_background.create_oval(x1, y1, x2, y2, fill=color.get(), outline=color.get(), width=brush_scale.get())
 
 
 def clear():
     # ReLoad in Canvas
-    canvas_background.create_image(0, 0, anchor=NW, image=image_input[-1])
+    canvas_background.create_image(0, 0, anchor="nw", image=image_input[-1])
+
+
+def slider_event_guidance(value):
+    guidance_counter_var.set(str(value))
+
+
+def slider_event_strength(value):
+    rounded_value = round(value, 2)
+    strength_counter_var.set(str(rounded_value))
+
+
+def slider_event_brush(value):
+    brush_counter_var.set(str(value))
 
 
 def start_thread(mode):
     # Disable generate buttons
-    gen_button_r["state"] = "disabled"
+    gen_button.configure(state="disabled")
     t = threading.Thread(target=generate)
     t.start()
 
 
 def generate():
     # Save mask image
-    x = Canvas.winfo_rootx(canvas_background)
-    y = Canvas.winfo_rooty(canvas_background)
-    w = Canvas.winfo_width(canvas_background)
-    h = Canvas.winfo_height(canvas_background)
+    x = tk.Canvas.winfo_rootx(canvas_background)
+    y = tk.Canvas.winfo_rooty(canvas_background)
+    w = tk.Canvas.winfo_width(canvas_background)
+    h = tk.Canvas.winfo_height(canvas_background)
 
     img = ImageGrab.grab((x, y, x + w, y + h))
     pixels = img.load()  # create the pixel map
@@ -67,21 +79,21 @@ def generate():
     img.save("images/mask.png")
 
     # Get values
-    values = {"Prompt": prompt_text_r.get(1.0, "end-1c"),
-              "Guidance scale": guidance_var_r.get(),
-              "Strength": strength_scale_r.get(),
-              "Image": input_im_text_r.get(),
-              "Output": f"images/inpainting/{output_text_r.get().replace(' ', '_')}"}
+    values = {"Prompt": prompt_text.textbox.get("0.0", "end"),
+              "Guidance scale": guidance_var.get(),
+              "Strength": strength_scale.get(),
+              "Image": input_im_text.get(),
+              "Output": f"images/inpainting/{output_text.get().replace(' ', '_')}"}
 
     # Start generation script
     inpainting(values=values, pipe=pipe)
 
     # Display generated image
-    new_image = PhotoImage(file=f"{values['Output']}.png")
-    canvas_render.create_image(0, 0, anchor=NW, image=new_image)
+    new_image = tk.PhotoImage(file=f"{values['Output']}.png")
+    canvas_render.create_image(0, 0, anchor="nw", image=new_image)
 
     # Reactivate generate buttons
-    gen_button_r["state"] = "normal"
+    gen_button.configure(state="normal")
 
 
 if __name__ == "__main__":
@@ -97,92 +109,119 @@ if __name__ == "__main__":
                                                               provider="DmlExecutionProvider", revision="onnx",
                                                               safety_checker=None)
 
+    # Graphical parameters
+    customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+    customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+
     # Main window
-    root = Tk()
-    root.title("Stable Chatoune")
-    root.geometry('1600x600')
+    root = customtkinter.CTk()
+    root.title("Stable UI Inpainting")
+    root.geometry('1600x800')
     root.resizable(False, False)
 
-    ## Render tab widgets ##
-    # Create frames
-    top_frame = Frame(root)
-    bot_frame = Frame(root)
-    inpaint_param_frame = Frame(top_frame)
-    inpaint_image_frame_left = Frame(top_frame, padding=(30, 0))
-    inpaint_image_frame_right = Frame(top_frame, padding=(30, 0))
-    button_frame = Frame(bot_frame)
-    top_frame.pack(side=TOP)
-    bot_frame.pack(side=BOTTOM)
-    inpaint_param_frame.pack(side=LEFT)
-    inpaint_image_frame_left.pack(side=LEFT)
-    inpaint_image_frame_right.pack(side=LEFT)
-    button_frame.pack()
-
     # Prompt
-    prompt_label_r = Label(inpaint_param_frame, text="Prompt", font=("Comic Sans MS", 15))
-    prompt_text_r = Text(inpaint_param_frame, width=60, height=5)
-    prompt_label_r.grid(column=0, row=1, sticky="W")
-    prompt_text_r.grid(column=0, row=2, columnspan=3)
+    prompt_label = customtkinter.CTkLabel(root, text="Prompt", text_font=("Arial", 15))
+    prompt_label.text_label.place(relx=0, rely=0.5, anchor="w")
+    prompt_text = customtkinter.CTkTextbox(root, width=500, height=100)
+    prompt_label.place(x=50, y=20)
+    prompt_text.place(x=50, y=50)
 
     # Guidance scale
-    guidance_var_r = DoubleVar()
-    guidance_var_r.set(7.5)
-    guidance_label_r = Label(inpaint_param_frame, text="Guidance scale", font=("Comic Sans MS", 15))
-    guidance_scale_r = tk.Scale(inpaint_param_frame, variable=guidance_var_r, from_=1, to=20, orient=HORIZONTAL,
-                                resolution=0.5, length=300)
-    guidance_label_r.grid(column=0, row=6, sticky="W")
-    guidance_scale_r.grid(column=1, row=6, columnspan=2, sticky="W")
+    guidance_var = tk.DoubleVar()
+    guidance_var.set(7.5)
+    guidance_label = customtkinter.CTkLabel(root, text="Guidance scale", text_font=("Arial", 15))
+    guidance_label.text_label.place(relx=0, rely=0.5, anchor="w")
+    guidance_scale = customtkinter.CTkSlider(root, variable=guidance_var, from_=1, to=20,
+                                             number_of_steps=38, width=200, command=slider_event_guidance)
+    guidance_scale.set(12)
+    guidance_counter_var = tk.StringVar(value="12.0")
+    guidance_counter = customtkinter.CTkLabel(root, textvariable=guidance_counter_var, text_font=("Arial", 15))
+    guidance_counter.text_label.place(relx=0, rely=0.5, anchor="w")
+    guidance_label.place(x=600, y=60)
+    guidance_scale.place(x=750, y=65)
+    guidance_counter.place(x=960, y=60)
 
     # Strength
-    strength_value_r = DoubleVar()
-    strength_value_r.set(0.75)
-    strength_label_r = Label(inpaint_param_frame, text="Strength", font=("Comic Sans MS", 15))
-    strength_scale_r = tk.Scale(inpaint_param_frame, variable=strength_value_r, from_=0, to=1, orient=HORIZONTAL,
-                                resolution=0.05, length=300)
-    strength_label_r.grid(column=0, row=7, sticky="W")
-    strength_scale_r.grid(column=1, row=7, columnspan=2, sticky="W")
+    strength_value = tk.DoubleVar()
+    strength_value.set(0.75)
+    strength_label = customtkinter.CTkLabel(root, text="Strength", text_font=("Arial", 15))
+    strength_label.text_label.place(relx=0, rely=0.5, anchor="w")
+    strength_scale = customtkinter.CTkSlider(root, variable=strength_value, from_=0, to=1,
+                                             number_of_steps=20, width=200, command=slider_event_strength)
+    strength_scale.set(0.75)
+    strength_counter_var = tk.StringVar(value="0.75")
+    strength_counter = customtkinter.CTkLabel(root, textvariable=strength_counter_var, text_font=("Arial", 15))
+    strength_counter.text_label.place(relx=0, rely=0.5, anchor="w")
+    strength_label.place(x=600, y=100)
+    strength_scale.place(x=750, y=105)
+    strength_counter.place(x=960, y=100)
 
     # Output file
-    output_label_r = Label(inpaint_param_frame, text="Image name", font=("Comic Sans MS", 15))
-    output_text_r = Entry(inpaint_param_frame, width=30)
-    output_text_r.insert(0, "example")
-    output_label_r.grid(column=0, row=8, sticky="W")
-    output_text_r.grid(column=1, row=8, sticky="W")
+    output_label = customtkinter.CTkLabel(root, text="Image name", text_font=("Arial", 15))
+    output_label.text_label.place(relx=0, rely=0.5, anchor="w")
+    output_text = customtkinter.CTkEntry(root, width=280)
+    output_text.insert(0, "example")
+    output_label.place(x=1050, y=100)
+    output_text.place(x=1200, y=100)
 
     # Input file
-    input_im_label_r = Label(inpaint_param_frame, text="Input image", font=("Comic Sans MS", 15))
-    input_im_text_r = Entry(inpaint_param_frame, width=50, state="disabled")
-    input_im_text_r.insert(0, "")
-    input_im_label_r.grid(column=0, row=9, sticky="W")
-    input_im_text_r.grid(column=1, row=9, sticky="W")
+    input_im_label = customtkinter.CTkLabel(root, text="Input image", text_font=("Arial", 15))
+    input_im_label.text_label.place(relx=0, rely=0.5, anchor="w")
+    input_im_text = customtkinter.CTkEntry(root, width=280, state="disabled")
+    input_im_text.insert(0, "")
+    load_btn = customtkinter.CTkButton(root, text="Import", command=browse_files, text_font=("Arial", 10), width=50)
+    input_im_label.place(x=1050, y=60)
+    input_im_text.place(x=1200, y=60)
+    load_btn.place(x=1500, y=60)
 
     # Generate
-    gen_button_r = tk.Button(inpaint_param_frame, text='Generate', command=lambda: start_thread("inpainting"),
-                             font=("Comic Sans MS", 30, "bold"), width=15)
-    gen_button_r.grid(column=0, row=10, columnspan=3, pady=25)
+    gen_button = customtkinter.CTkButton(root, text='Generate',
+                                         command=lambda: start_thread("inpainting"),
+                                         text_font=("Arial", 30, "bold"), width=300, height=80)
+    gen_button.place(x=650, y=450)
+
+    # Progress bar
+    prog_bar = customtkinter.CTkProgressBar(master=root, width=300)
+    prog_bar.set(0)
+    prog_bar.place(x=650, y=535)
 
     # Canvas for image input
-    canvas_background = Canvas(inpaint_image_frame_left, width=512, height=512, bg='white')
-    canvas_background.grid(column=0, row=0)
+    canvas_background = tk.Canvas(root, width=512, height=512, bg='white')
+    canvas_background.place(x=100, y=250)
 
     # Canvas buttons
-    load_btn = tk.Button(button_frame, text="Load image", command=browseFiles)
-    clear_btn = tk.Button(button_frame, text="Clear mask", command=clear)
     size_value = tk.IntVar(value=25)
-    paint_size = tk.Spinbox(button_frame, from_=2, to=50, textvariable=size_value)
     canvas_background.bind("<B1-Motion>", paint)
-    color = StringVar()
+    color = tk.StringVar()
     color.set("white")
-    color_btn1 = Radiobutton(button_frame, text="white", variable=color, value="white", state="normal")
-    color_btn1.pack(side=LEFT)
-    color_btn2 = Radiobutton(button_frame, text="black", variable=color, value="black")
-    color_btn2.pack(side=LEFT)
-    load_btn.pack(side=LEFT)
-    clear_btn.pack(side=LEFT)
-    paint_size.pack(side=LEFT, padx=10)
+    color_btn1 = customtkinter.CTkRadioButton(root, text="white", variable=color, value="white", state="normal",
+                                              text_font=("Arial", 10))
+    color_btn2 = customtkinter.CTkRadioButton(root, text="black", variable=color, value="black",
+                                              text_font=("Arial", 10))
+    color_btn1.place(x=100, y=210)
+    color_btn2.place(x=170, y=210)
+
+    # Paint brush size
+    brush_value = tk.DoubleVar()
+    brush_value.set(30)
+    brush_label = customtkinter.CTkLabel(root, text="Brush size", text_font=("Arial", 10))
+    brush_label.text_label.place(relx=0, rely=0.5, anchor="w")
+    brush_scale = customtkinter.CTkSlider(root, variable=brush_value, from_=1, to=50,
+                                          number_of_steps=49, width=150, command=slider_event_brush)
+    brush_scale.set(30)
+    brush_counter_var = tk.StringVar(value="30.0")
+    brush_counter = customtkinter.CTkLabel(root, textvariable=brush_counter_var, text_font=("Arial", 10))
+    brush_counter.text_label.place(relx=0, rely=0.5, anchor="w")
+    brush_label.place(x=250, y=207)
+    brush_scale.place(x=315, y=215)
+    brush_counter.place(x=465, y=208)
+
+    # Clear button
+    clear_btn = customtkinter.CTkButton(root, text="Clear mask", command=clear, width=50)
+    clear_btn.place(x=530, y=205)
 
     # Canvas for image output
-    canvas_render = Canvas(inpaint_image_frame_right, width=512, height=512, bg='white')
-    canvas_render.pack()
+    canvas_render = tk.Canvas(root, width=512, height=512, bg='white')
+    canvas_render.place(x=988, y=250)
 
     root.mainloop()
